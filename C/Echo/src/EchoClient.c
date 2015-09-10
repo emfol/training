de4257c95b4ca993ea2269dccd9e999ea3e560cc
Emanuel Fiuza <efiuza@avenuecode.com>
2015-09-10 13:53:45 -0300

    Socket Training in C...

---
+++ C/Echo/src/EchoClient.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main( int argc, char *argv[] ) {
+
+    puts( "Client... Oops!" );
+    return EXIT_SUCCESS;
+
+}
+
---
+++ C/Echo/src/EchoServer.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main( int argc, char *argv[] ) {
+
+    puts( "Server... Oops!" );
+    return EXIT_SUCCESS;
+
+}
+

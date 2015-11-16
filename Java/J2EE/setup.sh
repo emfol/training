24b8f2e49a55ab18d0cb234488b13394c102255e
Emanuel F. Oliveira <efiuza@me.com>
2015-11-15 22:51:14 -0200

    J2EE - Updated setup.sh to work with wget and curl...

--- Java/J2EE/setup.sh
+++ Java/J2EE/setup.sh
@@ -1,32 +1,54 @@
 #!/bin/sh
 
-JETTY_NAME="jetty-distribution-8.1.17.v20150415"
+JETTY_VERSION="8.1.17.v20150415"
+JETTY_NAME="jetty-distribution-$JETTY_VERSION"
 JETTY_ARCHIVE="$JETTY_NAME.tar.gz"
-JETTY_BASEURL="http://download.eclipse.org/jetty/8.1.17.v20150415/dist"
-JETTY_DEST="container"
+JETTY_BASEURL="http://download.eclipse.org/jetty/$JETTY_VERSION/dist"
+JETTY_DEST="target"
+
 TMPDIR=".cache"
 DWNDIR="$TMPDIR/downloads"
+RUNDIR=$(pwd)
 
-_abort() {
+_script_abort() {
     echo "Aborting..."
-    exit 1;
+    exit 1
+}
+
+_script_download_jetty() {
+    local RESULT
+    if command -v wget > /dev/null 2>&1
+    then
+        cd "$DWNDIR"
+        wget "$JETTY_BASEURL/$JETTY_ARCHIVE"
+        RESULT=$?
+        cd "$RUNDIR"
+    elif command -v curl > /dev/null 2>&1
+    then
+        curl -L -o "$DWNDIR/$JETTY_ARCHIVE" "$JETTY_BASEURL/$JETTY_ARCHIVE"
+        RESULT=$?
+    else
+        echo "Download tool not found... Please install wget or curl and try again."
+        RESULT=1
+    fi
+    return $RESULT
 }
 
 # create downloads dir...
-if [ ! -d "$DWNDIR" ]
+if [ ! -e "$DWNDIR" ]
 then
-    mkdir -p -- "$DWNDIR"
+    mkdir -p "$DWNDIR"
 fi
 
 # download jetty
 if [ ! -f "$DWNDIR/$JETTY_ARCHIVE" ]
 then
-    echo "Downloading Jetty..."
-    curl -L -o "$DWNDIR/$JETTY_ARCHIVE" -- "$JETTY_BASEURL/$JETTY_ARCHIVE"
+    echo "Trying to download Jetty..."
+    _script_download_jetty
     if [ $? -ne 0 ]
     then
-        rm -f "$DWNDIR/$JETTY_ARCHIVE"
-        _abort
+        rm -f "$DWNDIR/$JETTY_ARCHIVE" > /dev/null 2>&1
+        _script_abort
     fi
 fi
 
@@ -37,7 +59,7 @@ then
     tar xzf "$DWNDIR/$JETTY_ARCHIVE"
     if [ $? -ne 0 ]
     then
-        _abort
+        _script_abort
     fi
 fi
 
@@ -45,7 +67,8 @@ fi
 if [ ! -e "$JETTY_DEST" ] && [ -e "$JETTY_NAME" ]
 then
     echo "Moving files..."
-    mv -- "$JETTY_NAME" "$JETTY_DEST"
+    mv "$JETTY_NAME" "$JETTY_DEST"
 fi
 
 echo "Done!"
+

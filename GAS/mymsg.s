e77ee20c38a1c4acf37790328a17e63954756c21
Emanuel F. Oliveira <efiuza@me.com>
2016-06-12 21:03:51 -0300

    GNU Assembler Training... AT&T Assembly Syntax"

---
+++ GAS/mymsg.s
@@ -0,0 +1,77 @@
+/**
+ * @author Emanuel F. Oliveira
+ * @date 2016-06-12
+ * @description "This program prints a default message if no argument is passed..."
+ * @motivation "Training AT&T Assembly Syntax"
+ */
+
+    .global start
+
+    .text
+
+exit:
+    movl $1, %eax
+    int $0x80
+    ret
+
+write:
+    movl $4, %eax
+    int $0x80
+    ret
+
+strlen:
+    movl 4(%esp), %eax
+    movl %eax, %ecx
+  strlen_loop:
+    cmpb $0, (%eax)
+    je strlen_exit
+    inc %eax
+    jmp strlen_loop
+  strlen_exit:
+    subl %ecx, %eax
+    ret
+
+start:
+
+    pushl %ebp
+    movl %esp, %ebp
+    subl $4, %esp
+
+    /* check argc... */
+    cmpl $2, 4(%ebp)
+    jl start_stdMsg
+    movl $1, %ecx
+    movl 8(%ebp, %ecx, 4), %eax /* load EAX with argv[1] */
+    jmp start_stdFlw
+
+  start_stdMsg:
+    mov $msg, %eax
+
+  start_stdFlw:
+    movl %eax, -4(%ebp)
+
+    /* how long is the string? */
+    pushl %eax
+    call strlen
+    addl $4, %esp
+    /* now lets print it... */
+    pushl %eax
+    pushl -4(%ebp)
+    pushl $1
+    call write
+    addl $12, %esp
+
+    /* clean exit... */
+    pushl $0
+    call exit
+
+    /* playing safe... */
+    leave
+    ret
+
+
+    .data
+
+msg:
+.ascii "Nothing more than my default message...\n\0"
+
